cfd38b800e8689ea5b83692829a1f7a424eebd30
tainistranits <tainistranits@atomicmail.io>
2026-05-09 20:03:23 +0300

    фыы

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
 if [ "${RUN_IMPORT:-false}" = "true" ]; then
-  python manage.py import_db --flush --images
+  echo "RUN_IMPORT=true detected, loading fixture..."
+  python manage.py loaddata seed_data/current_db.json --verbosity=2
 fi
 
